14971f6093f9fbe8548cee4691c486f9da4fbeb4
qgh123456 <2104484868@qq.com>
2020-07-30 15:05:33 +0800

    用户模块

--- micro-role/src/main/java/com/atzlq/micro/role/MicroRoleApplication.java
+++ micro-role/src/main/java/com/atzlq/micro/role/MicroRoleApplication.java
@@ -1,9 +1,11 @@
 package com.atzlq.micro.role;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.atzlq.micro.user.**.mapper")
 public class MicroRoleApplication {
 
     public static void main(String[] args) {
--- micro-user/src/main/java/com/atzlq/micro/user/MicroUserApplication.java
+++ micro-user/src/main/java/com/atzlq/micro/user/MicroUserApplication.java
@@ -1,9 +1,11 @@
 package com.atzlq.micro.user;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.atzlq.micro.user.**.mapper")
 public class MicroUserApplication {
 
     public static void main(String[] args) {
