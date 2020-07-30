b2a0628736b5a3b6449eb0d427e557d05f4c75f7
qgh123456 <2104484868@qq.com>
2020-07-30 13:39:25 +0800

    用户模块

---
+++ micro-user/src/main/java/com/atzlq/micro/user/MicroUserApplication.java
@@ -0,0 +1,13 @@
+package com.atzlq.micro.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroUserApplication.class, args);
+    }
+
+}
