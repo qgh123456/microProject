4a70fca4337c67ba8c41a6c080904e215e163e52
qgh123456 <2104484868@qq.com>
2020-07-30 17:14:13 +0800

    添加common

---
+++ micro-common/src/main/java/com/atzlq/micro/common/config/SwaggerConfig.java
@@ -0,0 +1,78 @@
+package com.atzlq.micro.common.config;
+
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author lizihao
+ * @version 1.0
+ * @date 2020/3/10 17:32
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    /**
+     * 这个是除了被分组之外的都在这里显示(default)
+     * @return
+     */
+/*    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any()).build();
+    }*/
+
+    @Bean("用户服务")
+    public Docket userApis() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("用户服务")
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class)) // 所有标了API注解的才在文档中展示
+                .paths(PathSelectors.regex("/user.*")) // user下的所有请求
+                .build()
+                .apiInfo(userApiInfo())
+                .enable(true);
+    }
+
+    private ApiInfo userApiInfo() {
+        return new ApiInfoBuilder()
+                .title("用户服务接口文档")
+                .description("提供用户服务的文档")
+                .termsOfServiceUrl("http://http://www.bmsoft.com.cn/")
+                .version("1.0")
+                .build();
+    }
+
+
+    @Bean("角色服务")
+    public Docket roleApis() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("角色服务")
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class)) // 所有标了API注解的才在文档中展示
+                .paths(PathSelectors.regex("/role.*")) // role下的所有请求
+                .build()
+                .apiInfo(roleApiInfo())
+                .enable(true);
+    }
+
+    private ApiInfo roleApiInfo() {
+        return new ApiInfoBuilder()
+                .title("角色服务")
+                .description("提供角色服务的文档")
+                .termsOfServiceUrl("http://http://www.bmsoft.com.cn/")
+                .version("1.0")
+                .build();
+    }
+
+}
---
+++ micro-common/src/main/java/com/atzlq/micro/common/handler/CommonMetaObjectHandler.java
@@ -0,0 +1,31 @@
+package com.atzlq.micro.common.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+/**
+ * @ProjectName: microProject
+ * @Description:
+ * @Author: qiguohui
+ * @Date: 2020/7/30 16:51
+ */
+@Component
+@Slf4j
+public class CommonMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("start insert fill ....");
+        this.setFieldValByName("gmtCreate", new Date(), metaObject);//自动添加数据（插入）
+        this.setFieldValByName("gmtModified", new Date(), metaObject);//自动添加数据（插入）
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("start update fill ....");
+        this.setFieldValByName("gmtModified", new Date(), metaObject);//自动添加数据（修改）
+    }
+}
