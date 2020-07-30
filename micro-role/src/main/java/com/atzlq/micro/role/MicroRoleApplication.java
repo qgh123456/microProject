0dea20b03da317693eab94cfb7dfc008493b41ca
qgh123456 <2104484868@qq.com>
2020-07-30 13:44:51 +0800

    用户模块

---
+++ micro-role/src/main/java/com/atzlq/micro/role/MicroRoleApplication.java
@@ -0,0 +1,13 @@
+package com.atzlq.micro.role;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroRoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroRoleApplication.class, args);
+    }
+
+}
---
+++ micro-role/src/test/java/com/atzlq/micro/role/MicroRoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atzlq.micro.role;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroRoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
