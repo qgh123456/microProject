b622ae4bfc8a0b09d5e5359da018066de33f394f
qgh123456 <2104484868@qq.com>
2020-07-31 18:06:58 +0800

    pom

---
+++ micro-role/src/main/java/com/atzlq/micro/role/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.atzlq.micro.role.controller;
+
+/**
+ * @ProjectName: micro-project1
+ * @Description:
+ * @Author: qiguohui
+ * @Date: 2020/7/31 18:06
+ */
+public class TestController {
+}
