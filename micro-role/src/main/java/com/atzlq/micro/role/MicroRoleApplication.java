887f1a4aa6e85ff86a9c87c2feb0cb6c199a5cfe
qgh123456 <2104484868@qq.com>
2020-07-31 23:10:23 +0800

    完善框架

--- micro-role/src/main/java/com/atzlq/micro/role/MicroRoleApplication.java
+++ micro-role/src/main/java/com/atzlq/micro/role/MicroRoleApplication.java
@@ -3,13 +3,13 @@ package com.atzlq.micro.role;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 //@EnableFeignClients
 @MapperScan(basePackages = "com.atzlq.micro.role.**.dao")
 public class MicroRoleApplication {
