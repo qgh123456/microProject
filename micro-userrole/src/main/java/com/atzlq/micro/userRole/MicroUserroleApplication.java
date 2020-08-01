9c653455f1a997ffac05d9c24ea2c14de6e70b8f
qgh123456 <2104484868@qq.com>
2020-08-01 08:49:54 +0800

    完善框架

---
+++ micro-userrole/src/main/java/com/atzlq/micro/userRole/MicroUserroleApplication.java
@@ -0,0 +1,13 @@
+package com.atzlq.micro.userRole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroUserroleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroUserroleApplication.class, args);
+    }
+
+}
