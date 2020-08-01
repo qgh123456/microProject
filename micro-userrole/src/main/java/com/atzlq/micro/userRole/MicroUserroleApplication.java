43ab649f5ff7b10586c9ea371872cfea50732ddf
qgh123456 <2104484868@qq.com>
2020-08-01 10:21:26 +0800

    完善框架

--- micro-userrole/src/main/java/com/atzlq/micro/userRole/MicroUserroleApplication.java
+++ micro-userrole/src/main/java/com/atzlq/micro/userRole/MicroUserroleApplication.java
@@ -1,9 +1,16 @@
 package com.atzlq.micro.userRole;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableDiscoveryClient
+//@EnableFeignClients
+@MapperScan(basePackages = "com.atzlq.micro.userRole.**.dao")
 public class MicroUserroleApplication {
 
     public static void main(String[] args) {
