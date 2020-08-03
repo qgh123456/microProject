7336cbf4011add408fa238bfcb43f885158eb3dd
qgh123456 <2104484868@qq.com>
2020-08-03 14:39:55 +0800

    添加redis集群

---
+++ micro-common/common-cache/src/main/java/com/atzlq/micro/common/cache/RedisApplication.java
@@ -0,0 +1,18 @@
+//package com.atzlq.micro.common.cache;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+///**
+// * @ProjectName: micro-project
+// * @Description:
+// * @Author: qiguohui
+// * @Date: 2020/8/3 14:31
+// */
+//@SpringBootApplication
+//public class RedisApplication {
+//
+//    public static void main(String[] args) {
+//            SpringApplication.run(RedisApplication.class, args);
+//    }
+//}
