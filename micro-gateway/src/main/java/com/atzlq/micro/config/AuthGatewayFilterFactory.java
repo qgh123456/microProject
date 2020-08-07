c134c349110f12d1e9c4b73e55dc3b27eefd8f7c
qgh123456 <2104484868@qq.com>
2020-08-07 17:24:42 +0800

    sdf

--- micro-gateway/src/main/java/com/atzlq/micro/config/AuthGatewayFilterFactory.java
+++ micro-gateway/src/main/java/com/atzlq/micro/config/AuthGatewayFilterFactory.java
@@ -1,24 +1,24 @@
-package com.atzlq.micro.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.stereotype.Component;
-
-/**
- * @ProjectName: micro-project
- * @Description:
- * @Author: qiguohui
- * @Date: 2020/8/4 11:19
- */
-@Component
-public class AuthGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
-
-    @Autowired
-    private AuthGatewayFilter authGatewayFilter;
-
-    @Override
-    public GatewayFilter apply(Object config) {
-        return null;
-    }
-}
+//package com.atzlq.micro.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.cloud.gateway.filter.GatewayFilter;
+//import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * @ProjectName: micro-project
+// * @Description:
+// * @Author: qiguohui
+// * @Date: 2020/8/4 11:19
+// */
+//@Component
+//public class AuthGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
+//
+//    @Autowired
+//    private AuthGatewayFilter authGatewayFilter;
+//
+//    @Override
+//    public GatewayFilter apply(Object config) {
+//        return null;
+//    }
+//}
